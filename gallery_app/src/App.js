263babd4063b01c97e2b0af34cabcb0501d4152d
vpanameno <valerie.michelle@icloud.com>
2021-05-05 11:38:45 -0700

    Updated project files

---
+++ gallery_app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './index.css';
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+      <div className="main-header">
+    <div className="inner">
+      <h1 className="main-title">Gif Search</h1>
+    </div>
+  </div>
+  <div className="main-content">
+
+  </div>
+    </div>
+  );
+}
+}
+
+export default App;
