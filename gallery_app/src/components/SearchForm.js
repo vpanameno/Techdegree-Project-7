40e67c86207fe55a9ec67e6ab3d529958a6bc5a4
vpanameno <valerie.michelle@icloud.com>
2021-05-07 00:25:24 -0700

    dome

--- gallery_app/src/components/SearchForm.js
+++ gallery_app/src/components/SearchForm.js
@@ -17,6 +17,7 @@ class SearchForm extends Component {
         e.preventDefault();
         this.props.onSearch(this.query.value);
         let path = `/search/${this.query.value}`
+        this.props.history.push(path);
         e.currentTarget.reset();
     }
 
